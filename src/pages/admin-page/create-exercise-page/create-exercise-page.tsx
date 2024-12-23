39e6381820b3ebf772abb5caaee18f370be886e4
razdryzgULTIMATE <alphanomore2222@gmail.com>
2024-12-23 22:38:15 +0300

    Сделал запрет на ввод недопустимых символов при ручном способе создания упражнения

--- src/pages/admin-page/create-exercise-page/create-exercise-page.tsx
+++ src/pages/admin-page/create-exercise-page/create-exercise-page.tsx
@@ -1,4 +1,4 @@
-import {FC, useEffect, useState} from "react";
+import {ChangeEvent, Dispatch, FC, SetStateAction, useEffect, useRef, useState} from "react";
 import './create-exercise.css'
 import {NavigateFunction, useNavigate} from "react-router-dom";
 import axios from "axios";
@@ -25,6 +25,51 @@ export interface ResponseDifficulty {
     max_len: number,
     max_errors: number
 }
+
+const Manually: FC<{
+    selectedDif: ResponseDifficulty,
+    errors: string[], setErrors: Dispatch<SetStateAction<string[]>>, txtt: string,
+    setTxtt: Dispatch<SetStateAction<string>>
+}> = (props) => {
+    // const textAreaRef = useRef<HTMLTextAreaElement>(null); // Создаем ref для textarea
+    const handleChangeText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        if (props.selectedDif !== undefined) {
+            const txt = e.target.value;
+            const allowedSymbols: string[] = [];
+            // Собираем все допустимые символы
+            for (let i = 0; i < props.selectedDif.zones.length; i++) {
+                for (const symbols of props.selectedDif.zones[i].symbols) {
+                    allowedSymbols.push(...symbols); // Используем spread оператор для добавления символов
+                }
+            }
+
+            const str = allowedSymbols.join('');
+            const regex = new RegExp(`^[${str}]*$`); // Создаем регулярное выражение
+
+            // Проверяем, соответствует ли введенный текст допустимым символам
+            if (regex.test(txt) || txt === '') {
+                props.setTxtt(txt);
+                // props.setErrors([]); // Очищаем ошибки
+            }
+        } else {
+            props.setErrors(["Выберите уровень сложности!"]);
+        }
+    }
+
+    return (
+        <div className={'flex flex-col items-center justify-center'}>
+            <p className={'txt mb-2 text-xl'}>Текст упражнения</p>
+            <textarea id={'txt_ex'}
+                      key={1452}
+                      hidden={false}
+                      value={props.txtt}
+                // onChange={e => props.setTxtt(e.target.value)}
+                      onChange={(e) => handleChangeText(e)}
+                      className={'border-2 border-black w-80 h-20 p-2'}></textarea>
+        </div>
+    );
+}
+
 
 const createExercise: FC = () => {
     const [showAuto, setShowAuto] = useState(false);
@@ -144,20 +189,56 @@ const createExercise: FC = () => {
         );
     }
 // * Вручную
-    const Manually = () => {
-        return (
-            <div className={'flex flex-col items-center justify-center'}>
-                <p className={'txt mb-2 text-xl'}>Текст упражнения</p>
-                <textarea id={'txt_ex'}
-                          className={'border-2 border-black w-80 h-20 p-2'}></textarea>
-            </div>
-        );
-    }
+//     const Manually: FC<{difficult: ResponseDifficulty}> = (props) => {
+//         // const textAreaRef = useRef<HTMLTextAreaElement>(null); // Создаем ref для textarea
+//         const handleChangeText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+//             if (selectedDif !== undefined) {
+//                 const txt = e.target.value;
+//                 const allowedSymbols: string[] = [];
+//                 // Собираем все допустимые символы
+//                 for (let i = 0; i < selectedDif.zones.length; i++) {
+//                     for (const symbols of selectedDif.zones[i].symbols) {
+//                         allowedSymbols.push(...symbols); // Используем spread оператор для добавления символов
+//                     }
+//                 }
+//
+//                 const str = allowedSymbols.join('');
+//                 const regex = new RegExp(`^[${str}]*$`); // Создаем регулярное выражение
+//
+//                 // Проверяем, соответствует ли введенный текст допустимым символам
+//                 if (regex.test(txt) || txt === '') {
+//                     setTxtt(txt);
+//                     setErrors([]); // Очищаем ошибки
+//                 }
+//             } else {
+//                 setErrors(["Выберите уровень сложности!"]);
+//             }
+//         }
+//
+//         return (
+//             <div className={'flex flex-col items-center justify-center'}>
+//                 <p className={'txt mb-2 text-xl'}>Текст упражнения</p>
+//                 <textarea id={'txt_ex'}
+//                           key={1452}
+//                           hidden={false}
+//                         value={txtt}
+//                           onChange={e => setTxtt(e.target.value)}
+//                         // onChange={(e) => handleChangeText(e)}
+//                         className={'border-2 border-black w-80 h-20 p-2'}></textarea>
+//                 <input id={'txt_ex_1'}
+//                        className={'border-2 border-black w-80 h-20 p-2'}
+//                        hidden={true}
+//                        // value={txtt}
+//                        // onChange={(e) => handleChangeText(e)}
+//                 />
+//             </div>
+//         );
+//     }
 
 
     // console.log("RESPONSE DIF: \n", dif);
     const difficultList = dif.map((d) => <option key={d.name} value={d.name}>{d.name}</option>);
-    console.log(difficultList);
+    // console.log(difficultList);
     // * Обработчик для первой кнопки
     const handleAuto = () => {
         setShowAuto(true);
@@ -200,13 +281,14 @@ const createExercise: FC = () => {
                         {difficultList}
                     </select>
                 </div>
-                {selectedDif === undefined ? '' : <div className={"flex-col items-center justify-items-center mt-2 text-xl"}>
-                    <p>Минимальная длина: &nbsp; {selectedDif.min_len}&nbsp;символов</p>
-                    <p>Максимальная длина: &nbsp; {selectedDif.max_len}&nbsp;символов</p>
-                    <p>Время нажатия: &nbsp;{selectedDif.toggle_time} &nbsp;секунда</p>
-                    <p>Максимальное количество ошибок: &nbsp;{selectedDif.max_errors * 100}% от длины упражнения</p>
-                    <p>Допустимые символы: [ {selectedDif.zones.map(z => z.symbols.map(s => s + " "))}]</p>
-                </div>}
+                {selectedDif === undefined ? '' :
+                    <div className={"flex-col items-center justify-items-center mt-2 text-xl"}>
+                        <p>Минимальная длина: &nbsp; {selectedDif.min_len}&nbsp;символов</p>
+                        <p>Максимальная длина: &nbsp; {selectedDif.max_len}&nbsp;символов</p>
+                        <p>Время нажатия: &nbsp;{selectedDif.toggle_time} &nbsp;секунда</p>
+                        <p>Максимальное количество ошибок: &nbsp;{selectedDif.max_errors * 100}% от длины упражнения</p>
+                        <p>Допустимые символы: [ {selectedDif.zones.map(z => z.symbols.map(s => s + " "))}&nbsp;]</p>
+                    </div>}
                 <br/>
                 <div className={'flex flex-row justify-center space-x-5 mb-2'}>
                     <button onClick={handleAuto} className="bg-gray-300 px-4 py-2">Автоматический</button>
@@ -214,7 +296,9 @@ const createExercise: FC = () => {
                 </div>
                 <div className={'container items-center justify-items-center'}>
                     {showAuto && <Auto/>}
-                    {showManually && <Manually/>}
+                    {showManually &&
+                        <Manually selectedDif={selectedDif} txtt={txtt} setTxtt={setTxtt} setErrors={setErrors}
+                                  errors={errors}/>}
                     {
                         (showManually || showAuto) &&
                         <button onClick={() => save(showAuto)}
@@ -224,102 +308,4 @@ const createExercise: FC = () => {
             </div>
         </div>);
 }
-
-/*async function  save(navigate: NavigateFunction, showAuto: boolean, selected: string, difficulties: ResponseDifficulty[]){
-    // const selectElement: HTMLSelectElement = document.getElementById('select_difficult');
-    // const sel: string = selectElement.value;
-    const titleExInput: HTMLInputElement = document.getElementById("title_ex");
-    const titleEx:string = titleExInput.value;
-    let exer: CreateExercise;
-    const errs:string[] = [];
-    if (titleEx.length < 5 || titleEx.length > 20){
-        errs.push("Название упражнения должно быть длиной от 5 до 20 символов!")
-    }
-    if(selected === ''){
-        errs.push("Выберите уровень сложности!")
-    }
-    let difficults: ResponseDifficulty = null;
-    for (const difficulty of difficulties) {
-        if(selected === difficulty.name){
-            difficults = difficulty;
-        }
-    }
-    if(showAuto){
-        const lenInput: HTMLInputElement = document.getElementById('len_ex');
-        const len: number = +lenInput.value;
-
-        if(len > difficults.max_len || len < difficults.min_len){
-            errs.push(`Длина для генерации не входит в диапазон [${difficults.min_len}; ${difficults.max_len}]!`)
-        }
-
-        exer = {
-            exerciseName: titleEx,
-            difficultyLevelName: selected,
-            autoLength: len,
-            isAuto: true
-        };
-        console.log(exer);
-    }
-    else{
-        const txtArea: HTMLTextAreaElement = document.getElementById("txt_ex");
-        const txt: string = txtArea.value;
-        if(txt.length > difficults.max_len || txt.length < difficults.min_len){
-            errs.push(`Длина упражнения для ручного ввода не входит в диапазон [${difficults.min_len}; ${difficults.max_len}]!`)
-        }
-        const allowedSymbols:string[] = []
-        for (let i = 0; i < difficults.zones.length; i++) {
-            for (const symbols of difficults.zones[i].symbols) {
-                allowedSymbols.push(symbols);
-
-            }
-        }
-        const str = allowedSymbols.join('');
-        const regex = new RegExp(`^[${str}]*$`); // Создаем регулярное выражение
-        if(!regex.test(txt)){
-            errs.push("Текст не соответствует заданным зонам клавиатуры!")
-        }
-        exer = {
-            exerciseName: titleEx,
-            difficultyLevelName: selected,
-            exerciseText: txt,
-            isAuto: false
-        }
-
-    }
-    console.log(errs);
-    if(errs.length === 0){
-        console.log("POST EXERCISE: \n",exer);
-        try{
-            const response = await axios.post("http://localhost:8080/admin/create-exercise", exer, { headers: {
-                    'Authorization': 'Bearer ' + localStorage.getItem('token'),
-                    'Content-Type': 'application/json'
-                }})
-            console.log("POST RESPONSE: ", response.data);
-        }
-        catch(err){
-            console.log("POST ERROR! ", err);
-        }
-        navigate(-1);
-    }
-}
-
-// * Автоматический
-const Auto:FC = () => {
-    return(
-        <div>
-            <p className={'txt mb-2'}>Введите необходимую<br/> длину упражнения</p>
-            <input id='len_ex' type={'number'} className={'border-2 border-black'}/>
-        </div>
-    );
-}
-// * Вручную
-const Manually = () => {
-    return(
-        <div className={'flex flex-col items-center justify-center'}>
-            <p className={'txt mb-2 text-xl'}>Текст упражнения</p>
-            <textarea id={'txt_ex'} className={'border-2 border-black w-80 h-20 p-2'}></textarea>
-        </div>
-    );
-}*/
-
 export default createExercise;
